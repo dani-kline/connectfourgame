40d2b976851a3873dc6a301458af7a6c97ee5143
DaniKline <88793362+DaniKline@users.noreply.github.com>
2021-12-05 12:58:22 -0500

    Update connect4.js

--- connect4.js
+++ connect4.js
@@ -11,8 +11,8 @@ let HEIGHT = 6;
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
-/*   makeBoard: create in-JS board structure:
-     board = array of rows, each row is array of cells  (board[y][x])
+/**   makeBoard: create in-JS board structure:
+ *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
